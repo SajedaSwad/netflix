0ada89eec422e63af773dd2b155a31b6cdfb078a
sajeda swad <201047@ppu.edu.ps>
2024-01-19 08:51:20 +0200

    navigate between slides

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,17 +6,71 @@ import { MovieApiServiceService } from 'src/app/service/movie-api-service.servic
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent {
- constructor (private service:MovieApiServiceService){}
-bannerResult:any=[];
- ngOnInit(): void {
-  this.bannerData();
- }
- bannerData(){
-  this.service.bannerApiData().subscribe((result)=>{
-    console.log(result,'bannerresult');
-    this.bannerResult = result.results;
-    
-  })
- }
+  constructor(private service: MovieApiServiceService) {}
+
+  bannerResult: any[] = [];
+  activeIndex = 0;
+
+  ngOnInit(): void {
+    this.bannerData();
+  }
+
+  bannerData() {
+    this.service.bannerApiData().subscribe((result) => {
+      console.log(result, 'bannerresult');
+      this.bannerResult = result.results;
+    });
+  }
+
+  nextSlide() {
+    this.activeIndex = (this.activeIndex + 1) % this.bannerResult.length;
+  }
+
+  prevSlide() {
+    this.activeIndex = (this.activeIndex - 1 + this.bannerResult.length) % this.bannerResult.length;
+  }
 }
+
+
+// export class HomeComponent {
+//   constructor(private service: MovieApiServiceService) {}
+
+//   bannerResult: any[] = [];
+//   activeIndex = 0;
+
+//   ngOnInit(): void {
+//     this.bannerData();
+//   }
+
+//   bannerData() {
+//     this.service.bannerApiData().subscribe((result) => {
+//       console.log(result, 'bannerresult');
+//       this.bannerResult = result.results;
+//     });
+//   }
+
+//   nextSlide() {
+//     this.activeIndex = (this.activeIndex + 1) % this.bannerResult.length;
+//   }
+
+//   prevSlide() {
+//     this.activeIndex = (this.activeIndex - 1 + this.bannerResult.length) % this.bannerResult.length;
+//   }
+
+//   getCarouselItems() {
+//     const length = this.bannerResult.length;
+
+//     // Calculate indices for previous, current, and next slides
+//     const prevIndex = (this.activeIndex - 1 + length) % length;
+//     const nextIndex = (this.activeIndex + 1) % length;
+
+//     // Create an array with the three slides to display
+//     return [
+//       this.bannerResult[prevIndex],
+//       this.bannerResult[this.activeIndex],
+//       this.bannerResult[nextIndex],
+//     ];
+//   }
+// }
